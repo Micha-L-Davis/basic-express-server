612a0b44ed8aae289862dc89b686122749a057ec
Micha Davis <m.leigh.davis@gmail.com>
2022-03-22 14:42:04 -0700

    add dotenv, refactor to use server.start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { append } = require('express/lib/response');
+require('dotenv');
 const server = require('./src/server.js');
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+server.start(PORT);
