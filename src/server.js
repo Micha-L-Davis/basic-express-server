329da1d3b493e0411ffd76f6cd93bc4bd1678f90
Micha Davis <m.leigh.davis@gmail.com>
2022-03-22 16:36:48 -0700

    import packages and middleware, establish routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 'use strict';
 
-const express = require(express);
+const logRoute = require('./middleware/logger.js');
+const validate = require('./middleware/validator.js');
+const error404 = require('./error-handlers/404.js');
+const error500 = require('./error-handlers/500.js');
+const express = require('express');
 const app = express();
 
-// app.get person route
+
+
+function handleName(request, response, next) {
+  response.send({ name: request.query.name });
+}
+
+app.use(logRoute);
+
+app.get('/person', validate, handleName);
+
+app.use(error404);
+
+app.use(error500);
 
 module.exports = {
   app,
